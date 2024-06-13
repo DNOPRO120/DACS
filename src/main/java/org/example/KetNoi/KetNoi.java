4e98bf482761a4dd47779c00aaef78ffbadac99c
unknown <zokm12duy@gmail.com>
2024-06-14 02:47:03 +0700

    first

---
+++ src/main/java/org/example/KetNoi/KetNoi.java
@@ -0,0 +1,38 @@
+package org.example.KetNoi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class KetNoi {
+
+        private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+        private static final String DB_URL = "jdbc:mysql://localhost:3306/qlktx";
+        private static final String USER = "root";
+        private static final String PASS = "";
+
+        private Connection connection;
+    {
+            try {
+                Class.forName(JDBC_DRIVER);
+                connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+    }
+
+        public Connection getConnection() {
+            return connection;
+        }
+
+        public void closeConnection() {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+}
---
+++ src/main/java/org/example/OOP/Room.java
@@ -0,0 +1,22 @@
+package org.example.OOP;
+
+
+public class Room {
+    private String rid;
+
+    public Room() {
+
+    }
+
+    public Room(String rid) {
+        this.rid=rid;
+    }
+
+    public String getRid() {
+        return rid;
+    }
+
+    public void setRid(String rid) {
+        this.rid = rid;
+    }
+}
